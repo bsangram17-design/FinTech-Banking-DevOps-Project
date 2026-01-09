6c6595f56b034b878dbbda1e254efcc4e79fbb76
bsangram17-design <bsangram17@gmail.com>
2026-01-09 05:37:31 +0530

    Create security.tf

---
+++ terraform/security.tf
@@ -0,0 +1,17 @@
+resource "aws_security_group" "fintech_sg" {
+  name = "fintech-sg"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
