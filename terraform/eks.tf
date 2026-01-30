09389fc349b0def28a178d0174547aca2ff1db7d
bsangram17-design <bsangram17@gmail.com>
2026-01-30 17:50:52 +0530

    Create eks.tf

---
+++ terraform/eks.tf
@@ -0,0 +1,22 @@
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 20.0"
+
+  cluster_name    = "fintech-eks"
+  cluster_version = "1.29"
+
+  vpc_id     = aws_vpc.main.id
+  subnet_ids = [
+    aws_subnet.public_1.id,
+    aws_subnet.public_2.id
+  ]
+
+  eks_managed_node_groups = {
+    default = {
+      desired_size = 2
+      min_size     = 1
+      max_size     = 3
+      instance_types = ["t3.medium"]
+    }
+  }
+}
