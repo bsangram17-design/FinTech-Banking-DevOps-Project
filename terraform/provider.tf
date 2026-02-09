7861a48a7bae3771c49306b598a65fb2df1da366
bsangram17-design <bsangram17@gmail.com>
2026-02-10 05:25:21 +0530

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,12 +1,6 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-}
-
 provider "aws" {
   region = "ap-south-1"
 }
+
+data "aws_caller_identity" "current" {}
+data "aws_availability_zones" "available" {}
