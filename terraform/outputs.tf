c288789fd82ac0d6b6e281b14fc8719df9830b8e
bsangram17-design <bsangram17@gmail.com>
2026-02-16 05:54:05 +0530

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,11 +1,3 @@
 output "cluster_name" {
   value = module.eks.cluster_name
 }
-
-output "cluster_endpoint" {
-  value = module.eks.cluster_endpoint
-}
-
-output "configure_kubectl" {
-  value = "aws eks update-kubeconfig --region ap-south-1 --name ${module.eks.cluster_name}"
-}
