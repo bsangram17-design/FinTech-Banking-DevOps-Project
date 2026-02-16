f76d126d44dc01213a70c159143360783846eb63
bsangram17-design <bsangram17@gmail.com>
2026-02-16 05:53:35 +0530

    Update eks.tf

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -2,25 +2,25 @@ module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "20.8.5"
 
-  cluster_name    = var.cluster_name
+  cluster_name    = "fintech-eks"
   cluster_version = "1.29"
 
   vpc_id     = module.vpc.vpc_id
-  subnet_ids = module.vpc.private_subnets
+  subnet_ids = module.vpc.public_subnets
 
   enable_irsa = true
 
+  cluster_endpoint_public_access = true
+
   eks_managed_node_groups = {
     default = {
-      instance_types = [var.node_instance_type]
+      instance_types = ["t3.small"]
 
       min_size     = 1
-      max_size     = 3
-      desired_size = 2
+      max_size     = 1
+      desired_size = 1
 
-      capacity_type = "ON_DEMAND"
+      subnet_ids = module.vpc.public_subnets
     }
   }
-
-  cluster_endpoint_public_access = true
 }
