c901962eee5614a0c24e8072985c46d91bbc520f
bsangram17-design <bsangram17@gmail.com>
2026-02-16 05:52:20 +0530

    Update vpc.tf

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -5,19 +5,14 @@ module "vpc" {
   name = "fintech-vpc"
   cidr = "10.0.0.0/16"
 
-  azs             = slice(data.aws_availability_zones.available.names, 0, 2)
-  public_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
-  private_subnets = ["10.0.11.0/24", "10.0.12.0/24"]
+  azs = ["ap-south-1a", "ap-south-1b"]
 
-  enable_nat_gateway   = true
-  single_nat_gateway   = true
+  public_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
+
+  enable_nat_gateway = false
   enable_dns_hostnames = true
 
   public_subnet_tags = {
     "kubernetes.io/role/elb" = 1
-  }
-
-  private_subnet_tags = {
-    "kubernetes.io/role/internal-elb" = 1
   }
 }
