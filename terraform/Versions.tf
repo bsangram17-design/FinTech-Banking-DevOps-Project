e6eaa7f871a61444cd69345daa657c9fe9393350
bsangram17-design <bsangram17@gmail.com>
2026-02-16 05:46:36 +0530

    Update Versions.tf

--- terraform/Versions.tf
+++ terraform/Versions.tf
@@ -6,15 +6,5 @@ terraform {
       source  = "hashicorp/aws"
       version = ">= 5.40"
     }
-
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = ">= 2.25"
-    }
-
-    helm = {
-      source  = "hashicorp/helm"
-      version = ">= 2.12"
-    }
   }
 }
